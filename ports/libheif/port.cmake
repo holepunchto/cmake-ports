f461504937537bbedcaa5698d2cc392cf67f70e5
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-01-27 14:46:00 +0100

    More Windows fixes

--- ports/libheif/port.cmake
+++ ports/libheif/port.cmake
@@ -3,10 +3,16 @@ include_guard(GLOBAL)
 find_port(dav1d)
 find_port(libde265)
 
+if(WIN32)
+  set(lib heif.lib)
+else()
+  set(lib libheif.a)
+endif()
+
 declare_port(
   "github:strukturag/libheif@1.19.5"
   heif
-  BYPRODUCTS lib/libheif${CMAKE_STATIC_LIBRARY_SUFFIX}
+  BYPRODUCTS lib/${lib}
   DEPENDS
     dav1d
     de265
@@ -19,7 +25,7 @@ declare_port(
 
     "-DWITH_DAV1D=ON"
     "-DDAV1D_INCLUDE_DIR=${dav1d_PREFIX}/include"
-    "-DDAV1D_LIBRARY=${dav1d_PREFIX}/lib/libdav1d${CMAKE_STATIC_LIBRARY_SUFFIX}"
+    "-DDAV1D_LIBRARY=${dav1d_PREFIX}/lib/libdav1d.a"
 
     "-DWITH_LIBDE265=ON"
     "-DLIBDE265_INCLUDE_DIR=${de265_PREFIX}/include"
@@ -42,7 +48,7 @@ add_dependencies(heif ${heif})
 set_target_properties(
   heif
   PROPERTIES
-  IMPORTED_LOCATION "${heif_PREFIX}/lib/libheif${CMAKE_STATIC_LIBRARY_SUFFIX}"
+  IMPORTED_LOCATION "${heif_PREFIX}/lib/${lib}"
 )
 
 file(MAKE_DIRECTORY "${heif_PREFIX}/include")
