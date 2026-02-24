7301118c623474e9647431fbcdb9ead4b697e527
Kasper Isager Dalsgarð <kasperisager@hey.com>
2026-02-24 10:24:53 +0100

    Support running `autoreconf` from MSYS2

--- autoreconf.cmake
+++ autoreconf.cmake
@@ -2,14 +2,45 @@ if(EXISTS "configure")
   return()
 endif()
 
-find_program(
-  autoreconf
-  NAMES autoreconf
-  REQUIRED
-)
+set(env)
+
+if(CMAKE_HOST_WIN32)
+  find_path(
+    msys2
+    NAMES msys2.exe
+    PATHS "C:/tools/msys64"
+    REQUIRED
+  )
+
+  list(APPEND env --modify "PATH=path_list_prepend:${msys2}/usr/bin")
+
+  find_program(
+    bash
+    NAMES bash
+    PATHS "${msys2}/usr/bin"
+    REQUIRED
+    NO_DEFAULT_PATH
+  )
+
+  find_program(
+    autoreconf
+    NAMES autoreconf
+    PATHS "${msys2}/usr/bin"
+    REQUIRED
+    NO_DEFAULT_PATH
+  )
+
+  set(autoreconf ${bash} ${autoreconf})
+else()
+  find_program(
+    autoreconf
+    NAMES autoreconf
+    REQUIRED
+  )
+endif()
 
 execute_process(
-  COMMAND ${autoreconf} -vfi
+  COMMAND ${CMAKE_COMMAND} -E env ${env} ${autoreconf} -vfi
   WORKING_DIRECTORY "${WORKING_DIRECTORY}"
   RESULT_VARIABLE result
   ERROR_VARIABLE error
